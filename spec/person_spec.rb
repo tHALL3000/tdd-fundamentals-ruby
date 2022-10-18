fe33371dfcf840a5e5d2cf718564a63b19aab2e6
thall3000 <tiffanycodes.co@gmail.com>
2022-10-18 15:17:06 -0600

    First draft of test for Person class. Fails.

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,11 +1,15 @@
 require_relative "../lib/person"
 
 describe Person do
-  describe "#full_name" do
-    it "returns the first and last names concatenated" do
-      # Write your spec here.
-      # You can delete the pending line.
-      pending "Not written yet"
+  context "full_name" do
+    it "should return the first and last names concatenated" do
+      @first_name = first_name
+      @last_name = last_name
+
+      @full_name << @first_name << @last_name
+      result = "#{first_name} #{last_name}"
+
+      expect (@full_name).to eq(result)
     end
   end
 end
