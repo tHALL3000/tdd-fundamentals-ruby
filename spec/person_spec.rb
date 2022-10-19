e2a2adb43c885e97e5a7595d9522d22b316ae4c3
thall3000 <tiffanycodes.co@gmail.com>
2022-10-19 09:37:11 -0600

    Support from Jedi to achieve answer.

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -3,13 +3,15 @@ require_relative "../lib/person"
 describe Person do
   context "full_name" do
     it "should return the first and last names concatenated" do
-      @first_name = first_name
-      @last_name = last_name
+      @first_name = "Tiffany"
+      @last_name = "Hall"
+      test_person = Person.new(first_name: @first_name, last_name: @last_name)
 
-      @full_name << @first_name << @last_name
-      result = "#{first_name} #{last_name}"
+      my_expected_result = "#{@first_name} #{@last_name}"
 
-      expect (@full_name).to eq(result)
+      expect(test_person.full_name).to eq(my_expected_result)
+
     end
+
   end
 end
